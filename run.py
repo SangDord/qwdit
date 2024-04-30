b13578fc046b427306f3a450aadc462f81efb0e9
sangdord <sangdord07@gmail.com>
2024-04-30 10:23:46 +0300

    Updated requirements.txt

--- run.py
+++ run.py
@@ -1,7 +1,13 @@
 from qwdit import app
+from waitress import serve
+import logging
+from paste.translogger import TransLogger
 
+logger = logging.getLogger('waitress')
+logger.setLevel(logging.INFO)
 
 HOST = '127.0.0.1'
 PORT = 8080
 
-app.run(host=HOST, port=PORT)
+#app.run(host=HOST, port=PORT)
+serve(TransLogger(app, setup_console_handler=False), host=HOST, port=PORT)
